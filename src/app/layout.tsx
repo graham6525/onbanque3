ec03bb917f3173294309d4a67fed54b78bcfa5f7
graham6525 <grahamhome721@gmail.com>
2026-06-13 20:56:47 +0200

    jehf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   };
 
   return (
-    <html lang="fr">
+    <html lang="es">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
